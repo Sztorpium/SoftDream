dafa460840a4292abb93916cdb308a8b4bb3e1c9
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-08 14:24:47 +0100

    Create SoftDreamApplication main class

--- src/main/java/hu/softdream/SoftDreamApplication.java
+++ src/main/java/hu/softdream/SoftDreamApplication.java
@@ -1,4 +1,11 @@
 package hu.softdream;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SoftDreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoftDreamApplication.class, args);
+    }
 }
