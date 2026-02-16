1ff72c5c3a5c632b0d07da2c72b316c80e7b7043
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:48:56 +0100

    Declaring ReviewRepository interface

--- src/main/java/hu/softdream/repository/ReviewRepository.java
+++ src/main/java/hu/softdream/repository/ReviewRepository.java
@@ -1,4 +1,18 @@
 package hu.softdream.repository;
 
-public interface ReviewRepository {
+import hu.softdream.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByRoom_RoomId(Integer roomId);
+    List<Review> findByUser_UserId(Integer userId);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.room.roomId = :roomId")
+    Double findAverageRatingByRoomId(@Param("roomId") Integer roomId);
 }
