3050d1bc9f379419f74e7f66f5d05ec6607747e7
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:21:00 +0100

    Declaring BookingRequest dto (request)

--- src/main/java/hu/softdream/dto/request/BookingRequest.java
+++ src/main/java/hu/softdream/dto/request/BookingRequest.java
@@ -1,4 +1,28 @@
 package hu.softdream.dto.request;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookingRequest {
+
+    @NotNull(message = "A szobaszám kötelező!")
+    private Integer roomId;
+
+    @NotNull(message = "A bejelentkezése dátuma kötelező!")
+    @Future(message = "A bejelentkezés dátumának a jövőben kell lennie!")
+    private LocalDate checkIn;
+
+    @NotNull(message = "A kijelentkezés dátuma kötelező!")
+    @Future(message = "A kijelentkezés dátumának a jövőben kell lennie!")
+    private LocalDate checkOut;
 }
