5277763bf6d5cce4066aa9e12e9f04b0040a48d5
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:34:35 +0100

    Declaring RoomResponse dto (response)

--- src/main/java/hu/softdream/dto/response/RoomResponse.java
+++ src/main/java/hu/softdream/dto/response/RoomResponse.java
@@ -1,4 +1,24 @@
 package hu.softdream.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomResponse {
+    private Integer roomId;
+    private String roomNumber;
+    private Integer floor;
+    private String status;
+    private String type;
+    private BigDecimal basePrice;
+    private String description;
+    private Integer maxGuests;
+    private Double averageRating;
 }
