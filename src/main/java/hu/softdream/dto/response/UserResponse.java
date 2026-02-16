182adeb97b86f7fbb2da1993fe67d2255b5b0444
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:33:08 +0100

    Declaring UserResponse dto (response)

--- src/main/java/hu/softdream/dto/response/UserResponse.java
+++ src/main/java/hu/softdream/dto/response/UserResponse.java
@@ -1,4 +1,21 @@
 package hu.softdream.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserResponse {
+    private Integer userId;
+    private String username;
+    private String email;
+    private String phone;
+    private String role;
+    private LocalDateTime createdAt;
 }
