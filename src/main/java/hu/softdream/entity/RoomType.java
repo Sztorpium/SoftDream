3652493fa19b4abac34c9f5d814f0fe5bc339966
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:14:41 +0100

    Declaring the RoomType.java entity

--- src/main/java/hu/softdream/entity/RoomType.java
+++ src/main/java/hu/softdream/entity/RoomType.java
@@ -1,4 +1,33 @@
 package hu.softdream.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "room_types")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_type_id")
+    private Integer roomTypeId;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "base_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal basePrice;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
 }
