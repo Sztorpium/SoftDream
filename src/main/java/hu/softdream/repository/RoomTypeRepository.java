e3194fc01445bfe5c0b5e5412f37d6e3b740ae54
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:41:53 +0100

    Declaring RoomTypeRepository interface

--- src/main/java/hu/softdream/repository/RoomTypeRepository.java
+++ src/main/java/hu/softdream/repository/RoomTypeRepository.java
@@ -1,4 +1,12 @@
 package hu.softdream.repository;
 
-public interface RoomTypeRepository {
+import hu.softdream.entity.RoomType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
+    Optional<RoomType> findByName(String name);
 }
