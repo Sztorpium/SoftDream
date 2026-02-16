17236c7dae98e67c8dbfc4571e0806d886dc34e3
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:44:04 +0100

    Declaring RoomStatusRepository interface

--- src/main/java/hu/softdream/repository/RoomStatusRepository.java
+++ src/main/java/hu/softdream/repository/RoomStatusRepository.java
@@ -1,4 +1,12 @@
 package hu.softdream.repository;
 
-public interface RoomStatusRepository {
+import hu.softdream.entity.RoomStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoomStatusRepository extends JpaRepository<RoomStatus, Integer> {
+    Optional<RoomStatus> findByName(String name);
 }
