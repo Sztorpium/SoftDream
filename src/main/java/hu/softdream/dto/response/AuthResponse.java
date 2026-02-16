7eb08a1903dc921f0092ca15ff1b8edd844d865e
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:30:49 +0100

    Declaring AuthResponse dto (response)

--- src/main/java/hu/softdream/dto/response/AuthResponse.java
+++ src/main/java/hu/softdream/dto/response/AuthResponse.java
@@ -1,4 +1,19 @@
 package hu.softdream.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private Integer userId;
+    private String username;
+    private String email;
+    private String role;
 }
