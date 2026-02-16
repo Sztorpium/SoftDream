ea154a94061d9b8dc33ff940071ef7fe9a3c69fa
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:37:05 +0100

    Declaring RoleRepository interface

--- src/main/java/hu/softdream/repository/RoleRepository.java
+++ src/main/java/hu/softdream/repository/RoleRepository.java
@@ -1,4 +1,12 @@
 package hu.softdream.repository;
 
-public interface RoleRepository {
+import hu.softdream.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
 }
