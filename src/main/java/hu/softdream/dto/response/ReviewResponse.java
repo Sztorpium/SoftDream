60673303a328c7fcdfb7a4bc3a1a9f57ecc08080
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:39:16 +0100

    Declaring ReviewResponse dto (response)

--- src/main/java/hu/softdream/dto/response/ReviewResponse.java
+++ src/main/java/hu/softdream/dto/response/ReviewResponse.java
@@ -1,4 +1,23 @@
 package hu.softdream.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ReviewResponse {
+    private Integer reviewId;
+    private Integer userId;
+    private String username;
+    private Integer roomId;
+    private String roomNumber;
+    private Integer rating;
+    private String comment;
+    private LocalDateTime createdAt;
 }
