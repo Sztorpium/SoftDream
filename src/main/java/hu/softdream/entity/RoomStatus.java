60cf1a6e803eb02f28c5ae325c0bff59a6cf71e2
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:12:20 +0100

    Declaring the RoomStatus.java entity

--- src/main/java/hu/softdream/entity/RoomStatus.java
+++ src/main/java/hu/softdream/entity/RoomStatus.java
@@ -1,4 +1,26 @@
 package hu.softdream.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "room_status")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RoomStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_status_id")
+    private Integer roomStatusId;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String name; // AVAILABLE, BOOKED, MAINTENANCE
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
 }
