41cce3996871d66630faa5a533a95c6a2ead774b
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:10:23 +0100

    Declaring the UserAuth.java entity

--- src/main/java/hu/softdream/entity/UserAuth.java
+++ src/main/java/hu/softdream/entity/UserAuth.java
@@ -1,4 +1,32 @@
 package hu.softdream.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_auth")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserAuth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_id")
+    private Integer authId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
 }
