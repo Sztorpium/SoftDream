e5ca5eb05059c56c5281b491897070cfdada62f2
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:39:43 +0100

    Declaring RoomRepository interface

--- src/main/java/hu/softdream/repository/RoomRepository.java
+++ src/main/java/hu/softdream/repository/RoomRepository.java
@@ -1,4 +1,25 @@
 package hu.softdream.repository;
 
-public interface RoomRepository {
+import hu.softdream.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+    Optional<Room> findByRoomNumber(String roomNumber);
+    List<Room> findByRoomStatus_Name(String statusName);
+    List<Room> findByRoomType_RoomTypeId(Integer roomTypeId);
+
+    @Query("SELECT r FROM Room r WHERE r.roomId NOT IN " +
+            "(SELECT b.room.roomId FROM Booking b WHERE " +
+            "(b.checkIn <= :checkOut AND b.checkOut >= :checkIn) AND " +
+            "b.status = 'CONFIRMED')")
+    List<Room> findAvailableRooms(@Param("checkIn") LocalDate checkIn,
+                                  @Param("checkOut") LocalDate checkOut);
 }
