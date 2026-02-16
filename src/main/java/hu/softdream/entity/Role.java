39c71a6342d43289114fa5a21a8b3ce7a2c10811
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:03:15 +0100

    Declaring the Role entity

--- src/main/java/hu/softdream/entity/Role.java
+++ src/main/java/hu/softdream/entity/Role.java
@@ -1,4 +1,27 @@
 package hu.softdream.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String desciption;
 }
