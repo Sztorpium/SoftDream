9974e571bad0a8cacc04a1eb53a3a834a220219c
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:33:59 +0100

    Declaring UserAuthRepository interface

--- src/main/java/hu/softdream/repository/UserAuthRepository.java
+++ src/main/java/hu/softdream/repository/UserAuthRepository.java
@@ -1,4 +1,11 @@
 package hu.softdream.repository;
 
-public interface UserAuthRepository {
+import hu.softdream.entity.UserAuth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserAuthRepository extends JpaRepository<UserAuth, Integer> {
+    Optional<UserAuth> findByUser_Username(String username);
+    Optional<UserAuth> findByUser_UserId(Integer userId);
 }
