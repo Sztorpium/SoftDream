743f02f252b1fb32fe37c5f43444226c63abe276
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 18:46:47 +0100

    Declaring BookingRepository interface

--- src/main/java/hu/softdream/repository/BookingRepository.java
+++ src/main/java/hu/softdream/repository/BookingRepository.java
@@ -1,4 +1,25 @@
 package hu.softdream.repository;
 
-public interface BookingRepository {
+import hu.softdream.entity.Booking;
+import hu.softdream.entity.enums.BookingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    List<Booking> findByUser_UserId(Integer userId);
+    List<Booking> findByRoom_RoomId(Integer roomId);
+    List<Booking> findByStatus(BookingStatus status);
+
+    @Query("SELECT b FROM Booking b WHERE b.room.roomId = :roomId AND " +
+            "((b.checkIn <= :checkOut AND b.checkOut >= :checkIn)) AND " +
+            "b.status = 'CONFIRMED'")
+    List<Booking> findConflictingBookings(@Param("roomId") Integer roomId,
+                                          @Param("checkIn") LocalDate checkIn,
+                                          @Param("checkOut") LocalDate checkOut);
 }
