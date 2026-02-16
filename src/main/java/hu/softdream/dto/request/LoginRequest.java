088e533c6439e7e8c19abd7db25120b0221df4f7
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-16 19:11:03 +0100

    Declaring LoginRequest dto (request)

--- src/main/java/hu/softdream/dto/request/LoginRequest.java
+++ src/main/java/hu/softdream/dto/request/LoginRequest.java
@@ -1,4 +1,20 @@
 package hu.softdream.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+
+    @NotBlank(message = "A felhasználónév kötelező!")
+    private String username;
+
+    @NotBlank(message = "A jelszó kötelező!")
+    private String password;
 }
