e0ef2a39d983e5b6c8d4ab7a0113c3ab21cd3219
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-28 14:54:52 +0100

    Declaring the ResourceNotFoundException.java (Exception handling)

--- src/main/java/hu/softdream/exception/ResourceNotFoundException.java
+++ src/main/java/hu/softdream/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package hu.softdream.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
