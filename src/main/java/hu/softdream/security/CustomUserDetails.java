fcdb8999be1c0fe920eac134c6c535427a2a8a14
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-28 14:27:49 +0100

    Declaring the CustomUserDetails.java

--- src/main/java/hu/softdream/security/CustomUserDetails.java
+++ src/main/java/hu/softdream/security/CustomUserDetails.java
@@ -1,4 +1,66 @@
 package hu.softdream.security;
 
-public class CustomUserDetails {
+import hu.softdream.entity.UserAuth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserAuth userAuth;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_" + userAuth.getRole().getName())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return userAuth.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return userAuth.getUser().getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Integer getUserId() {
+        return userAuth.getUser().getUserId();
+    }
+
+    public String getEmail() {
+        return userAuth.getUser().getEmail();
+    }
+
+    public String getRole() {
+        return userAuth.getRole().getName();
+    }
+
 }
