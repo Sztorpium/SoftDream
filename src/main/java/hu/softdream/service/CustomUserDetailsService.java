b77aa230407522679c8c92786bc2692bc3199f27
Potam-5 <21c_pocsait@nyirszikszi.hu>
2026-02-28 14:32:26 +0100

    Declaring the CustomUserDetailsService.java

--- src/main/java/hu/softdream/service/CustomUserDetailsService.java
+++ src/main/java/hu/softdream/service/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package hu.softdream.service;
 
-public class CustomUserDetailsService {
+
+import hu.softdream.entity.UserAuth;
+import hu.softdream.repository.UserAuthRepository;
+import hu.softdream.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+        private final UserAuthRepository userAuthRepository;
+
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            UserAuth userAuth = userAuthRepository.findByUser_Username(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("A felhasználó nem található: " + username));
+
+            return new CustomUserDetails(userAuth);
+        }
 }
