ed7022b1e18aec41b4d8c17628314ae2428ea4a1
Sztorpium <21c_pasztorm@nyirszikszi.hu>
2026-03-24 16:06:38 +0100

    refactor(frontend): switch router to outlet-based ProtectedRoute

--- frontend/src/app/Router.jsx
+++ frontend/src/app/Router.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AppShell from "../components/AppShell";
 import Home from "../pages/Home";
 import About from "../pages/About";
@@ -7,7 +7,7 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MyBookings from "../pages/MyBookings";
 import NotFound from "../pages/NotFound";
-import ProtectedRoute from "../auth/ProtectedRoute";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function AppRouter() {
     return (
@@ -15,26 +15,15 @@ export default function AppRouter() {
             <Routes>
                 <Route path="/" element={<Home />} />
 
-                <Route
-                    path="/about"
-                    element={
-                        <ProtectedRoute>
-                            <About />
-                        </ProtectedRoute>
-                    }
-                />
-
-                <Route
-                    path="/my-bookings"
-                    element={
-                        <ProtectedRoute>
-                            <MyBookings />
-                        </ProtectedRoute>
-                    }
-                />
-
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+
+                {/* Protected routes */}
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/my-bookings" element={<MyBookings />} />
+                </Route>
+
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </AppShell>
