b0879904806d69f3f944bdfb29c1e77d0bdace67
Sztorpium <21c_pasztorm@nyirszikszi.hu>
2026-03-27 10:31:44 +0100

    feat(frontend): add a Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./app/Router";
-import { AuthProvider } from "./context/AuthContext";
+import App from "./App";
+import { theme } from "./app/theme";
 
-export default function App() {
-  return (
-    <AuthProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
-        <AppRouter />
+        <App />
       </BrowserRouter>
-    </AuthProvider>
-  );
-}
+    </ThemeProvider>
+  </React.StrictMode>
+);
